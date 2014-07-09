057967909a3fef37db2b07b251f149c4a25edfef
Weston Renoud <wrenoud@gmail.com>
2014-07-08 21:51:58 -0300

    Basic route and template for 'refs' resource

--- app/routes/refs.js
+++ app/routes/refs.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function(){
+		return [
+			{
+				title:"Something important",
+				year: "2010",
+				author:"Weston Renoud",
+			},
+			{
+				title:"Less important",
+				year: "2009",
+				author:"W Renoud",
+			}
+		];
+	},
 });
