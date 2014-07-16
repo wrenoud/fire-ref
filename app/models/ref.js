b4929d6333aa8a0c0acad69bca2171e9116b3daa
Weston Renoud <wrenoud@gmail.com>
2014-07-16 00:31:39 -0300

    moved FIXTURE data and added application adapter

--- app/models/ref.js
+++ app/models/ref.js
@@ -1,5 +1,27 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-  
+var ref = DS.Model.extend({
+  title: DS.attr(),
+  year: DS.attr(),
+  author: DS.attr(),
+
 });
+
+ref.reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      title:"Something important",
+      year: "2010",
+      author:"Weston Renoud",
+    },
+    {
+      id: 2,
+      title:"Less important",
+      year: "2009",
+      author:"W Renoud",
+    }
+  ]
+});
+
+export default ref
--- app/routes/refs.js
+++ app/routes/refs.js
@@ -2,19 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(){
-		return [
-			{
-				id: 1,
-				title:"Something important",
-				year: "2010",
-				author:"Weston Renoud",
-			},
-			{
-				id: 2,
-				title:"Less important",
-				year: "2009",
-				author:"W Renoud",
-			}
-		];
-	},
+		return this.store.find('ref');
+	}
 });
