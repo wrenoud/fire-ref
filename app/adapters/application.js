382b14e6d27f8228daeb5c52ff7a3a60b5a5e769
Weston Renoud <wrenoud@gmail.com>
2014-11-27 15:12:22 -0400

    Local storage adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,26 +3,30 @@ import DS from 'ember-data';
 //export default DS.RESTAdapter.extend({
 //});
 
-export default DS.Adapter.extend({
-  find: function(store, type, id) {
-    var client = Ember.Application.client;
-
-    if(type.contains(':user:')){
-      return client.getAccountInfo({},function(error, account){
-          var dets = account.json();
-          dets.id = account.uid;
-          store.load(type, account.uid, dets);
-          return dets;
-        })
-    }
-  },
-  findAll:  function(store, type) {
-    var client = Ember.Application.client;
-    if(type.toString().indexOf(':user:')>=0){
-      return client.getAccountInfo({},function(error, account){
-          console.log(account.uid);
-          return [account];
-        });
-    }
-  }
+export default DS.LSAdapter.extend({
+    namespace: 'wrenoud/fire-ref'
 });
+
+// export default DS.Adapter.extend({
+//   find: function(store, type, id) {
+//     var client = Ember.Application.client;
+
+//     if(type.contains(':user:')){
+//       return client.getAccountInfo({},function(error, account){
+//           var dets = account.json();
+//           dets.id = account.uid;
+//           store.load(type, account.uid, dets);
+//           return dets;
+//         })
+//     }
+//   },
+//   findAll:  function(store, type) {
+//     var client = Ember.Application.client;
+//     if(type.toString().indexOf(':user:')>=0){
+//       return client.getAccountInfo({},function(error, account){
+//           console.log(account.uid);
+//           return [account];
+//         });
+//     }
+//   }
+// });
